7f071f3c2d7705faba6b5dd883d3b694d2e381ae
Gabriel Turlea <gabrielturlea27@gmail.com>
2022-01-26 07:35:56 +0200

    layout lnding page

--- assets/js/main-v1.js
+++ assets/js/main-v1.js
@@ -1,19 +1,16 @@
 let body = document.querySelector('body');
 let main = document.querySelector('#main');
-let content = document.querySelector('#content');
-let initialize = document.querySelector('#initialize');
+
 let sectionFirstLeft = document.querySelector('#section-first-left');
 let sectionFirstRight = document.querySelector('#section-first-right');
+let mainImg = document.querySelector('#main-img');
+
 let compositeAnime = document.querySelectorAll('.composite-anime');
 let descend = document.querySelector('.descend');
-let messageMe = document.querySelector('.message-me');
-
 
 let menuItems = document.querySelectorAll('.menu-item');
-let menuItemsImgs = document.querySelectorAll('.menu-item-imgs');
 
 
-let mainabsolute = document.querySelector('#main-absolute');
 // let mainMatrix = document.querySelector('#main-matrix');
 // let matrixSpans = document.querySelectorAll('.terminal');
 // let overlay = document.querySelector('#overlay');
@@ -21,13 +18,15 @@ let checkboxes = document.querySelectorAll('input[type=checkbox]');
 // var svgimages = document.querySelectorAll('.svg-image');
 
 markCheckboxes();
-firstImgOpacity();
+
 scrollUpScreen();
 
 
 
 if (matchMedia) {
-    const mq = window.matchMedia("(max-width: 1189px)");
+    const mq = window.matchMedia("(max-width: 1124px)");
+    defaultanimation();
+    menuitemsanimation()
     mq.addListener(WidthChange);
     WidthChange(mq);
   }
@@ -48,8 +47,8 @@ if (matchMedia) {
 
 function initialpc(){
 
-    defaultanimation();
-    menuitemsanimation()
+    
+    
     // showall();
 }
 
@@ -58,51 +57,50 @@ function initialphone(){
 }
 
 function defaultanimation(){
-    let init1 = document.querySelector('.init1');
-    let init2 = document.querySelector('.init2');
-    let init3 = document.querySelector('.init3');
-    let init4 = document.querySelector('.init4');
-    let init5 = document.querySelector('.init5');
-    let init6 = document.querySelector('.init6');
-    let init7 = document.querySelector('.init7');
-    let init8 = document.querySelector('.init8');
+    let nav = document.querySelector('nav');
+    let mainIm = mainImg;
+    let spanElements = document.querySelectorAll('.span-elements');
+    let descendRotate = document.querySelector('#descend-rotate');
+
+    initialAnimation = gsap.timeline({ defaults:{duration: 1.2}})
+    initialAnimation.to(nav, { ease: "power2", opacity: 1, delay:0});
+    initialAnimation.from(nav, { ease: "power2", y: "-10%", delay:-1.2});
+    initialAnimation.to(mainIm, { ease: "power3", opacity: 1, duration:2, delay:-.3});
+    initialAnimation.from(mainIm, { ease: "power3", y: "+10%", duration:2, delay:-2.2});
+    initialAnimation.to(spanElements, { ease: "power2", opacity: 1, delay:-.2, stagger: 0.3});
+    initialAnimation.to(descendRotate, { ease: "power3", opacity: 1, duration:2, delay:-.3});
+    initialAnimation.from(descendRotate, { ease: "power3", y: "+10%", duration:2, delay:-2.2});
+        
+    // setTimeout(startTimer, 10000)
+    setTimeout(function(){
+        startTimer();
+    }, 4500); 
+
     
+    function startTimer() {
+        let darkSvg = document.querySelectorAll('.dark-svg');
+        let counter = 0;
+        timer = setInterval(function() {
+            if(counter == darkSvg.length){
+                counter = 0;
+            }
+            changeSvg(darkSvg[counter])
+            counter ++;
 
-        initialAnimation = gsap.timeline({ defaults:{duration: .1}})
-        initialAnimation.to(init1, { ease: "expo", opacity: 1, delay:0});
-        initialAnimation.to(init2, { ease: "expo", opacity: 1, delay:0, duration:.7});
-        initialAnimation.to(init2, { ease: "expo", opacity: 0, delay:0});
-        initialAnimation.to(init3, { ease: "expo", opacity: 1, delay:0});
-        initialAnimation.to(init4, { ease: "expo", opacity: 1, delay:0});
-        initialAnimation.to(init5, { ease: "expo", opacity: 1, delay:0, duration:.7});
-        initialAnimation.to(init5, { ease: "expo", opacity: 0, delay:0});
-        initialAnimation.to(init6, { ease: "expo", opacity: 1, delay:0});
-        initialAnimation.to(init7, { ease: "expo", opacity: 1, delay:0});
-        initialAnimation.to(init8, { ease: "expo", opacity: 1, delay:0, duration:.7});
-     
-        initialAnimation.to(initialize, { ease: "expo", opacity: 0, delay:0, duration:.1});
-        initialAnimation.to(mainabsolute, { ease: "power3", opacity: 1, duration:1.2, delay:-.1});
-        initialAnimation.to(initialize, { ease: "expo", zIndex: -1, duration:.01});
-        
-        initialAnimation.to(content, { ease: "power3", opacity: 1, duration:1.6, delay:-1.4});
-        initialAnimation.from(sectionFirstRight, { ease: "power3", y: "-10%", duration:.8, delay:-.4});
-        initialAnimation.to(sectionFirstRight, { ease: "power3", opacity: 1, duration:1.2, delay:-.8});
-        
-        initialAnimation.to(mainabsolute, { ease: "power3", opacity:.4, duration:1.2, delay:-1.2});
+        }, 4000);
 
-        
-
-        initialAnimation.from(sectionFirstLeft, { ease: "power3", x: "-10%", duration:.8, delay:-.4});
-        initialAnimation.to(sectionFirstLeft, { ease: "power3", opacity: 1, duration:1.2, delay:-.8});
-        
-        initialAnimation.to(menuItems, { ease: "power3", opacity: 1,  stagger:.1, delay:-.2});
-        initialAnimation.to(compositeAnime, { ease: "power3", opacity: 1, duration:.6});
-        initialAnimation.to(descend, { ease: "power3", opacity: 1, duration:.6});
-        initialAnimation.from(messageMe, { ease: "power3", y: "+10%", duration:.8, delay:-.4});
-        initialAnimation.to(messageMe, { ease: "power3", opacity: 1, duration:1.2, delay:-.8});
-        
-        // initialAnimation.to(mainabsolute, { ease: "power3", filter: "brightness(80%)", duration:.2, delay:-.1});
-        // initialanimation();
+        function changeSvg(svg){
+            console.log(svg);
+            darkSvg.forEach(function(f){
+                f.style.opacity = 0;
+            
+            })
+            setTimeout(function(){
+                svg.style.opacity = 1;
+            }, 500); 
+  
+        }
+    }
 }
 
 function showall(){
@@ -126,22 +124,22 @@ function menuitemsanimation(){
     menuItems.forEach(function(e){
         
         // hover animation
-        tl = gsap.timeline({ defaults:{duration: .1}})
-        e.addEventListener('mouseenter', function(event){
-            elem = event.target.querySelector('.menu-item-imgs')
-            imgs = elem.querySelectorAll('img');
+        // tl = gsap.timeline({ defaults:{duration: .1}})
+        // e.addEventListener('mouseenter', function(event){
+        //     elem = event.target.querySelector('.menu-item-imgs')
+        //     imgs = elem.querySelectorAll('img');
             
-            tl.to(imgs[0], { ease: "power3", opacity: 0, duration:.2});
-            tl.to(imgs[1], { ease: "power3", opacity: 1, duration:.3, delay:-.2});
-        })
+        //     tl.to(imgs[0], { ease: "power3", opacity: 0, duration:.2});
+        //     tl.to(imgs[1], { ease: "power3", opacity: 1, duration:.3, delay:-.2});
+        // })
 
-        e.addEventListener('mouseleave', function(event){
-            elem = event.target.querySelector('.menu-item-imgs')
-            imgs = elem.querySelectorAll('img');
+        // e.addEventListener('mouseleave', function(event){
+        //     elem = event.target.querySelector('.menu-item-imgs')
+        //     imgs = elem.querySelectorAll('img');
     
-            tl.to(imgs[1], { ease: "power3", opacity: 0, duration:.2});
-            tl.to(imgs[0], { ease: "power3", opacity: 1, duration:.3, delay:-.2});
-        })
+        //     tl.to(imgs[1], { ease: "power3", opacity: 0, duration:.2});
+        //     tl.to(imgs[0], { ease: "power3", opacity: 1, duration:.3, delay:-.2});
+        // })
 
         // on click event (scroll)
         e.addEventListener('click', function(){
@@ -180,8 +178,8 @@ function menuitemsanimation(){
     //display initial homepage
     
 
-      tl = gsap.timeline({ defaults:{duration: 3}})
-      tl.to(main, { ease: "expo", opacity: 1});
+    //   tl = gsap.timeline({ defaults:{duration: 3}})
+    //   tl.to(main, { ease: "expo", opacity: 1});
      
 
     //display monitor animation
@@ -255,12 +253,7 @@ function markCheckboxes(){
    })
 }
 
-function firstImgOpacity(){
-    menuItemsImgs.forEach(function(e){
-        let firstImg = e.querySelector('img');
-        firstImg.style.opacity = 1;
-    })
-}
+
 
 //upscreen
 function scrollUpScreen(){
